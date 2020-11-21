1dd905426d7ed00dbbc32784dc616a527f25052e
developer <developer@example.com>
2020-11-21 08:50:28 +0300

    --

--- cms/views/view-enter.php
+++ cms/views/view-enter.php
@@ -7,6 +7,7 @@
 <head>
 	<title>Cms</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/theme.css.php">
 </head>
 <body>
